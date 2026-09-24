3398a4013962a62ddbde84f11e10016a81c5c141
Naveen-A18 <2006naveen18@gmail.com>
2026-09-24 16:56:41 +0530

    data

--- app.py
+++ app.py
@@ -2,11 +2,32 @@ import mysql.connector
 from flask import Flask,render_template,request
 
 app =Flask(__name__)
-db=mysql.connector.connect(
+db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="root",
-    database="food_ordering",
-    )
+    password="Naveen@5559",
+    database="food_ordering"
+)
+
+@app.route("/")
+def food():
+    cursor =db.cursor()
+    cursor.execute("SELECT* FROM foods")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template("foods.html",foods=data)
+
+@app.route("/add")
+def add():
+    pass
+
+@app.route("/edit")
+def edit_food():
+    pass
+
+@app.route("/delete")
+def delete_food():
+    pass
+
 if __name__== "__main__":
     app.run(debug=True)
